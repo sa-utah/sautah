04066ade2f673217bebbde0e094f2adb18e8317d
Nic Johnson <iheartredsox@gmail.com>
2020-09-17 19:34:45 -0600

    Change Meetings to Forestry template

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,42 @@
+const util = require('util');
+
+const sortObject = o => Object.keys(o).sort().reduce((r, k) => (r[k] = o[k], r), {})
+
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('_src/assets');
   eleventyConfig.addPassthroughCopy('_src/favicon.ico');
+  eleventyConfig.addFilter("meetingLocations", meetings => {
+    let locations = meetings.reduce((acc, curr) => {
+      let location = curr.location;
+      if(!acc.hasOwnProperty(location)) {
+        acc[location] = 0;
+      }
+      acc[location]++;
+      return acc;
+    }, {});
+
+    locations = sortObject(locations);
+    
+    console.log(locations);
+
+    locations = Object.keys(locations).map(k => {
+      return { name: k } 
+    });
+
+    locations.forEach(location => {
+      meetings.forEach(meeting => {
+        if (!location.hasOwnProperty('meetings')) {
+          location.meetings = [];
+        }
+        if (location.name === meeting.location) {
+          location.meetings.push(meeting);
+        }
+      });
+    });
+    
+    return locations;
+  });
+
   return {
     dir: {
       input: '_src'
